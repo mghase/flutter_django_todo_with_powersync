6a5016d3db38d08465a2923d0fcdd751fb2318a3
sadick mghase <mghase.sadick1@gmail.com>
2024-10-28 09:23:45 +0300

    Update app_config_template.dart

--- django_todolist/lib/app_config_template.dart
+++ django_todolist/lib/app_config_template.dart
@@ -2,6 +2,6 @@
 // Edit lib/app_config.dart and enter your Django and PowerSync project details.
 class AppConfig {
   // These are defaults when using the [self-host local demo](https://github.com/powersync-ja/self-host-demo/tree/main/demos/django)
-  static const String djangoUrl = 'http://192.168.0.202:6061';
-  static const String powersyncUrl = 'http://192.168.0.202:8181';
+  static const String djangoUrl = 'http://ip:6061';
+  static const String powersyncUrl = 'http://ip:8181';
 }
